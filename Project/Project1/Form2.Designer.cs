295796c5bb3380723e00d3c0d44d7c1d1ac1a38b
Tín Lưu <107022820+tinluuVTL@users.noreply.github.com>
2022-08-28 21:21:29 +0700

    Add files via upload

---
+++ Project/Project1/Form2.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace LoginScreen
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bauhaus 93", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.label1.Location = new System.Drawing.Point(266, 62);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 63);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "HKT";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.UseCompatibleTextRendering = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bauhaus 93", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.label2.Location = new System.Drawing.Point(13, 124);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(575, 67);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Chào mừng đến với nhóm HKT";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.UseCompatibleTextRendering = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bauhaus 93", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.label3.Location = new System.Drawing.Point(13, 202);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(243, 135);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nhật Huynh\r\nMinh Khang\r\nVăn Tín";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::LoginScreen.Properties.Resources.Ảnh_nền_máy_tính_cực_đẹp_Gearvn;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(620, 335);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Project/Project1/frmLogin.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginScreen
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtUserName.Text))
+            {
+                if (!string.IsNullOrEmpty(txtpassword.Text))
+                {
+
+                    if (txtUserName.Text == "hktdemo" && txtpassword.Text == "hkt123")
+                    {
+                        new Form2().Show();
+                        this.Hide();
+
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("Tài Khoản hoặc mật khẩu sai. Kiểm tra lại");
+                        txtUserName.Clear();
+                        txtpassword.Clear();
+                        txtUserName.Focus();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Mật Khẩu Chưa Nhập", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtpassword.Focus();
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show("Tài khoản Chưa Nhập", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtUserName.Focus();
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            txtUserName.Clear();
+            txtpassword.Clear();
+            txtUserName.Focus();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void picb_Click(object sender, EventArgs e)
+        {
+            if(txtpassword.PasswordChar=='*')
+            {
+                pica.BringToFront();
+                txtpassword.PasswordChar = '\0';
+            }    
+        }
+
+        private void pica_Click(object sender, EventArgs e)
+        {
+            if (txtpassword.PasswordChar == '\0')
+            {
+                picb.BringToFront();
+                txtpassword.PasswordChar = '*';
+            }
+        }
+    }
+}
